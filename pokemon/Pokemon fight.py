17952e1255fc941f27e0ed3a7b2288cb25a87375
Caetano <84238339+dalestee@users.noreply.github.com>
2022-03-26 18:51:53 +0100

    Add files via upload

--- pokemon/Pokemon fight.py
+++ pokemon/Pokemon fight.py
@@ -1,17 +1,10 @@
 import random
 import csv
 from damage import damage
+from donnes import *
 
-def start():
-    Pokemon1N = input('Nom pokemon? ')
-    Pokemon1Item = input('item? ')
-    if Pokemon1Item == '':
-        Pokemon1Item = None
-    Pokemon2N = input('Nom pokemon adversaire? ')
-    Pokemon2Item = input('item? ')
-    if Pokemon2Item == '':
-        Pokemon2Item = None
-    return [pokemon_data(Pokemon1N), Pokemon1Item], [pokemon_data(Pokemon2N), Pokemon2Item]
+def Find_Poke(Poke):
+    return [pokemon_data(Poke)]
     
 def pokemon_data(Nom):  
     with open('pokemon-gen1-data.csv', 'r') as csv_file:
@@ -27,190 +20,217 @@ def Poke_Attack(PA,PD,Attack_Power,Attack_Type,Attack_Tod,Item = 1):
     else:
         Stab = 1
         
-    if PD[0][1] == 'WATER':
+    if PD[0][0] == 'WATER':
         if Attack_Type in Water_R:
             Type *= 0.5
         elif Attack_Type in Water_W:
             Type *= 2
-    if PD[0][1] == 'FIRE':
+    if PD[0][0] == 'FIRE':
         if Attack_Type in Fire_R:
             Type *= 0.5
         elif Attack_Type in Fire_W:
             Type *= 2
-    if PD[0][1] == 'STEEL':
-        if Attack_Type in Steel_R:
+    if PD[0][0] == 'STEEL':
+        if Attack_Type == "POISON":
+            Type *= 0
+        elif Attack_Type in Steel_R:
             Type *= 0.5
         elif Attack_Type in Steel_W:
             Type *= 2
-    if PD[0][1] == 'GRASS':
+    if PD[0][0] == 'GRASS':
         if Attack_Type in Grass_R:
             Type *= 0.5
         elif Attack_Type in Grass_W:
             Type *= 2
-    if PD[0][1] == 'GHOST':
-        if Attack_Type in Ghost_R:
+    if PD[0][0] == 'GHOST':
+        if Attack_Type == "FIGHTING" or Attack_Type == "NORMAL":
+            Type *= 0
+        elif Attack_Type in Ghost_R:
             Type *= 0.5
         elif Attack_Type in Ghost_W:
             Type *= 2
-    if PD[0][1] == 'BUG':
+    if PD[0][0] == 'BUG':
         if Attack_Type in Bug_R:
             Type *= 0.5
         elif Attack_Type in Bug_W:
             Type *= 2
-    if PD[0][1] == 'NORMAL':
-        if Attack_Type in Normal_R:
+    if PD[0][0] == 'NORMAL':
+        if Attack_Type == "GHOST":
+            Type *= 0 
+        elif Attack_Type in Normal_R:
             Type *= 0.5
         elif Attack_Type in Normal_W:
             Type *= 2
-    if PD[0][1] == 'POISON':
+    if PD[0][0] == 'POISON':
         if Attack_Type in Poison_R:
             Type *= 0.5
         elif Attack_Type in Poison_W:
             Type *= 2
-    if PD[0][1] == 'ELECTRIC':
+    if PD[0][0] == 'ELECTRIC':
         if Attack_Type in Electric_R:
             Type *= 0.5
         elif Attack_Type in Electric_W:
             Type *= 2
-    if PD[0][1] == 'GROUND':
-        if Attack_Type in Ground_R:
+    if PD[0][0] == 'GROUND':
+        if Attack_Type == "ELECTRIC":
+            Type *= 0
+        elif Attack_Type in Ground_R:
             Type *= 0.5
         elif Attack_Type in Ground_W:
             Type *= 2
-    if PD[0][1] == 'FAIRY':
-        if Attack_Type in Fairy_R:
+    if PD[0][0] == 'FAIRY':
+        if Attack_Type == "DRAGON":
+            Type *= 0
+        elif Attack_Type in Fairy_R:
             Type *= 0.5
         elif Attack_Type in Fairy_W:
             Type *= 2
-    if PD[0][1] == 'FIGHTING':
+    if PD[0][0] == 'FIGHTING':
         if Attack_Type in Fighting_R:
             Type *= 0.5
         elif Attack_Type in Fighting_W:
             Type *= 2
-    if PD[0][1] == 'PSYCHIC':
+    if PD[0][0] == 'PSYCHIC':
         if Attack_Type in Psychic_R:
             Type *= 0.5
         elif Attack_Type in Psychic_W:
             Type *= 2
-    if PD[0][1] == 'ROCK':
+    if PD[0][0] == 'ROCK':
         if Attack_Type in Rock_R:
             Type *= 0.5
         elif Attack_Type in Rock_W:
             Type *= 2
-    if PD[0][1] == 'ICE':
+    if PD[0][0] == 'ICE':
         if Attack_Type in Ice_R:
             Type *= 0.5
         elif Attack_Type in Ice_W:
             Type *= 2
-    if PD[0][1] == 'DRAGON':
+    if PD[0][0] == 'DRAGON':
         if Attack_Type in Dragon_R:
             Type *= 0.5
         elif Attack_Type in Dragon_W:
             Type *= 2
-    if PD[0][1] == 'FLYING':
-        if Attack_Type in Flying_R:
+    if PD[0][0] == 'FLYING':
+        if Attack_Type == "GROUND":
+            Type *= 0
+        elif Attack_Type in Flying_R:
             Type *= 0.5
         elif Attack_Type in Flying_W:
             Type *= 2
-    if PD[0][1] == 'DARK':
-        if Attack_Type in Dark_R:
+    if PD[0][0] == 'DARK':
+        if Attack_Type == "PSYCHIC":
+            Type *= 0
+        elif Attack_Type in Dark_R:
             Type *= 0.5
         elif Attack_Type in Dark_W:
-            Type *= 2
-            
-    elif PD[0][2] == 'none':
+            Type *= 2 
+    if PD[0][1] == 'none':
         if Attack_Tod == 'SP':
             return damage(100, Attack_Power, int(PA[0][8]), int(PD[0][9]), Stab, Type)
         else:
             return damage(100, Attack_Power, int(PA[0][6]), int(PD[0][7]), Stab, Type)
     else:
-        if PD[0][2] == 'WATER':
+        if PD[0][1] == 'WATER':
             if Attack_Type in Water_R:
                 Type *= 0.5
             elif Attack_Type in Water_W:
                 Type *= 2
-        if PD[0][2] == 'FIRE':
+        if PD[0][1] == 'FIRE':
             if Attack_Type in Fire_R:
                 Type *= 0.5
             elif Attack_Type in Fire_W:
                 Type *= 2
-        if PD[0][2] == 'STEEL':
-            if Attack_Type in Steel_R:
+        if PD[0][0] == 'STEEL':
+            if Attack_Type == "POISON":
+                Type *= 0
+            elif Attack_Type in Steel_R:
                 Type *= 0.5
             elif Attack_Type in Steel_W:
                 Type *= 2
-        if PD[0][2] == 'GRASS':
+        if PD[0][1] == 'GRASS':
             if Attack_Type in Grass_R:
                 Type *= 0.5
             elif Attack_Type in Grass_W:
                 Type *= 2
-        if PD[0][2] == 'GHOST':
-            if Attack_Type in Ghost_R:
+        if PD[0][0] == 'GHOST':
+            if Attack_Type == "FIGHTING" or Attack_Type == "NORMAL":
+                Type *= 0
+            elif Attack_Type in Ghost_R:
                 Type *= 0.5
             elif Attack_Type in Ghost_W:
                 Type *= 2
-        if PD[0][2] == 'BUG':
+        if PD[0][1] == 'BUG':
             if Attack_Type in Bug_R:
                 Type *= 0.5
             elif Attack_Type in Bug_W:
                 Type *= 2
-        if PD[0][2] == 'NORMAL':
-            if Attack_Type in Normal_R:
+        if PD[0][1] == 'NORMAL':
+            if Attack_Type == "GHOST":
+                Type *= 0
+            elif Attack_Type in Normal_R:
                 Type *= 0.5
             elif Attack_Type in Normal_W:
                 Type *= 2
-        if PD[0][2] == 'POISON':
+        if PD[0][1] == 'POISON':
             if Attack_Type in Poison_R:
                 Type *= 0.5
             elif Attack_Type in Poison_W:
                 Type *= 2
-        if PD[0][2] == 'ELECTRIC':
+        if PD[0][1] == 'ELECTRIC':
             if Attack_Type in Electric_R:
                 Type *= 0.5
             elif Attack_Type in Electric_W:
                 Type *= 2
-        if PD[0][2] == 'GROUND':
-            if Attack_Type in Ground_R:
+        if PD[0][1] == 'GROUND':
+            if Attack_Type == 'ELECTRIC':
+                Type *= 0
+            elif Attack_Type in Ground_R:
                 Type *= 0.5
             elif Attack_Type in Ground_W:
                 Type *= 2
-        if PD[0][2] == 'FAIRY':
-            if Attack_Type in Fairy_R:
+        if PD[0][1] == 'FAIRY':
+            if Attack_Type == "DRAGON":
+                Type *= 0
+            elif Attack_Type in Fairy_R:
                 Type *= 0.5
             elif Attack_Type in Fairy_W:
                 Type *= 2
-        if PD[0][2] == 'FIGHTING':
+        if PD[0][1] == 'FIGHTING':
             if Attack_Type in Fighting_R:
                 Type *= 0.5
             elif Attack_Type in Fighting_W:
                 Type *= 2
-        if PD[0][2] == 'PSYCHIC':
+        if PD[0][1] == 'PSYCHIC':
             if Attack_Type in Psychic_R:
                 Type *= 0.5
             elif Attack_Type in Psychic_W:
                 Type *= 2
-        if PD[0][2] == 'ROCK':
+        if PD[0][1] == 'ROCK':
             if Attack_Type in Rock_R:
                 Type *= 0.5
             elif Attack_Type in Rock_W:
                 Type *= 2
-        if PD[0][2] == 'ICE':
+        if PD[0][1] == 'ICE':
             if Attack_Type in Ice_R:
                 Type *= 0.5
             elif Attack_Type in Ice_W:
                 Type *= 2
-        if PD[0][2] == 'DRAGON':
+        if PD[0][1] == 'DRAGON':
             if Attack_Type in Dragon_R:
                 Type *= 0.5
             elif Attack_Type in Dragon_W:
                 Type *= 2
-        if PD[0][2] == 'FLYING':
-            if Attack_Type in Flying_R:
+        if PD[0][1] == 'FLYING':
+            if Attack_Type == "GROUND":
+                Type *= 0
+            elif Attack_Type in Flying_R:
                 Type *= 0.5
             elif Attack_Type in Flying_W:
                 Type *= 2
-        if PD[0][2] == 'DARK':
-            if Attack_Type in Dark_R:
+        if PD[0][1] == 'DARK':
+            if Attack_Type == "PSYCHIC":
+                Type *= 0
+            elif Attack_Type in Dark_R:
                 Type *= 0.5
             elif Attack_Type in Dark_W:
                 Type *= 2
@@ -218,6 +238,8 @@ def Poke_Attack(PA,PD,Attack_Power,Attack_Type,Attack_Tod,Item = 1):
         return damage(100, Attack_Power, int(PA[0][8]), int(PD[0][9]), Stab, Type)
     else:
         return damage(100, Attack_Power, int(PA[0][6]), int(PD[0][7]), Stab, Type)    
-Poke = start()
-Poke0 = Poke[0]
-Poke1 = Poke[1]
+Poke0 = Find_Poke("Charizard")
+Poke1 = Find_Poke("Blastoise")
+print(Poke0)
+print(Poke1)
+print(Poke_Attack(Poke0,Poke1,80,"NORMAL","SP",Item = 1))
